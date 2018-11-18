8927f6088cfee6a755979407de96948efd303fef
DariaBee <bobadaria@yahoo.com>
2018-11-18 14:09:41 +0100

    Dzien 1 python + 1 czesc zadania

---
+++ dzien1.py
@@ -0,0 +1,95 @@
+"""imie = input("Jak masz na imie")
+imie = imie.strip( )
+dlugosc = len(imie)
+ostatni_litera_imienia = imie[dlugosc-1]
+print("Cześć")
+print(imie.upper())
+print(imie[-1:])
+print("Twoje imie ma: " + str(dlugosc) + " liter")
+print("Twoja ostatnia litera imienia to : " + ostatni_litera_imienia
+      """
+
+"""imie = input("Jak masz na imie")
+imie = imie.strip( )
+dlugosc = len(imie)
+ostatni_litera_imienia = imie[dlugosc-1]
+print("Cześć")
+print(imie.upper())
+ostatnia_litera = imie[-1:]
+if(ostatnia_litera == 'a'):
+    print("Witam Panią")
+else:
+    print("Witam Pana")
+
+wzorF = "TempF = 32 + 9/5 * TempC"
+print(wzorF)
+
+TempC = input("Podaj temprature w stopniach Celcjusza:")
+TempC = int(TempC)
+TempF = 32 + 9/5 * TempC
+print(TempF)
+
+
+wzorC = "TempC = 5/9 * (TempF - 32)"
+
+print(wzorC)
+
+TempF = input("Podaj temprature w stopniach Fahrenheita:")
+
+TempF = int(TempF)
+
+TempC = 5/9 * (TempF - 32)
+
+print(TempC)
+
+
+dlugosc_boku_a = input("Podaj długość boku a:")
+
+dlugosc_boku_a = int(dlugosc_boku_a)
+
+dlugosc_boku_b = input("Podaj długość boku b:")
+
+dlugosc_boku_b = int(dlugosc_boku_b)
+
+wzor = "a * b"
+
+print(wzor)
+
+pole_powierzchni = dlugosc_boku_a * dlugosc_boku_b
+
+print(pole_powierzchni)
+
+
+r = input("Podaj dłogość promienia:")
+
+r = int(r)
+
+wzor = "3.14 * r**2"
+
+print(wzor)
+
+pole_powierzchni = 3.14 * r ** 2
+
+print(pole_powierzchni) """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
