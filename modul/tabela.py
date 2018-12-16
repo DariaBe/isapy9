c96e482d12c85a3f9339baa5796ca43b997fce24
DariaBee <bobadaria@yahoo.com>
2018-12-16 23:19:23 +0100

    Modul tabeli

---
+++ modul/tabela.py
@@ -0,0 +1,7 @@
+def wartosci_w_tabeli (wpisy):
+
+    for wpis in wpisy:
+        print(('+' + '-' * 12 + '+' + '-' * 30 + '+'))
+        print('| ' + wpis['data'] + ' ' + '| ' + wpis['tresc'])
+
+    print(('+' + '-' * 12 + '+' + '-' * 30 + '+'))
