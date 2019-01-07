b826d75353944566daef87a3684901efbf19f696
DariaBee <bobadaria@yahoo.com>
2019-01-07 18:08:19 +0100

    Aktualizacja

--- modul/tabela.py
+++ modul/tabela.py
@@ -5,46 +5,4 @@ def wartosci_w_tabeli (wpisy):
         print('| ' + wpis['data'] + ' ' + '| ' + wpis['tresc'])
 
     print(('+' + '-' * 12 + '+' + '-' * 30 + '+'))
-#
-# ndeks = 0
-# for litera in 'Kotek':
-#     print(litera)
-#     break
-#     indeks += 1
-#
-# print(indeks)
-# i
-#
-# i = 100
-# while i >= 100:
-#     print(i)
-# i -= 1
-# # print(i)
-# # import requests
-# a = 5
-# b = 6
-# a == 7
-# b = 8
-#
-# print(a)
 
-from pprint import pprint
-#
-# for l in range(0, 99, 33):
-#     print(l)
-# # wieek = 30
-# #
-# def okresl_grupe(wieek):
-#     if wieek < 30:
-#         grupa_osoby = 'm'
-#     elif 30 <= wieek < 60:
-#         grupa_osoby = 's'
-#     else:
-#         grupa_osoby = 'st'
-#
-# okresl_grupe(wieek)
-# print(okresl_grupe)
-
-# print(grupa_osoby)
-#
-# imie = 'olA'
